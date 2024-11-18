c493be8d67f3bc03bce0c5138ae3bba7a4fc9d3b
Milos Gavranovic <mgavranovic7@gmail.com>
2024-11-18 17:57:42 +0100

    Implemented config struct for cmds for easier pagination which fixed the bug from the previous commit

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ import (
 	"os"
 )
 
-var currentUrl string = "https://pokeapi.co/api/v2/location-area?offset=0&limit=20"
-var limit = 1
+var (
+	currentUrl string = "https://pokeapi.co/api/v2/location-area?offset=0&limit=20"
+)
 
+// Could use pointers maybe the value doesnt update aaa
 // could use bool params for next and previous (if true go next ...)
 // Getting the API data
-func getPokeAPI(url string, showLocations bool) (PokeData, error) {
+func getPokeAPI(url string, showLocations bool) (*PokeData, error) {
 	res, err := http.Get(url)
 	if err != nil {
-		return PokeData{}, err
+		return &PokeData{}, err
 	}
 
 	defer res.Body.Close()
@@ -24,26 +26,26 @@ func getPokeAPI(url string, showLocations bool) (PokeData, error) {
 	decoder := json.NewDecoder(res.Body)
 	if err := decoder.Decode(&data); err != nil {
 		fmt.Println("Error decoding json:", err)
-		return PokeData{}, err
+		return &PokeData{}, err
 	}
+
 	if showLocations {
-		// fmt.Println(data)
 		for _, v := range data.Results {
 			fmt.Printf("The names of the locations: %s\n", v.Name)
 		}
 	}
-	return data, nil
+	return &data, nil
 }
 
 // Commands struct
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(c *Config) error
 }
 
 // Commands callbacks
-func cmdHelp() error {
+func cmdHelp(c *Config) error {
 	fmt.Println("This is the list of commands you can use.")
 	for cmd := range cliCmd {
 		fmt.Println("- " + cliCmd[cmd].name + ": " + cliCmd[cmd].description)
@@ -51,32 +53,42 @@ func cmdHelp() error {
 	return fmt.Errorf("Error HELP")
 }
 
-func cmdExit() error {
+func cmdExit(c *Config) error {
 	fmt.Println("Test EXIT")
 	os.Exit(0)
 	// I have to figure out how to make an optional return, as this one is not going to happen due to exiting the program
 	return fmt.Errorf("Error EXIT")
 }
 
-func cmdMap() error {
-	data, err := getPokeAPI(currentUrl, true)
+func cmdMap(c *Config) error {
+	url := currentUrl
+	if c.Next != nil {
+		url = *c.Next
+	} else {
+	}
+
+	data, err := getPokeAPI(url, true)
 	if err != nil {
 		return err
 	}
-	currentUrl = data.Next
+
+	c.Next = &data.Next
+	c.Previous = &data.Previous
+
 	return nil
 }
 
-// The problem here is the url gets updated after cmd map is ran which changes the current url to the next one, and when cmd mapb is ran, it takes in that next url before it updates it properly (as the url is updated after the getPokeAPI function returns the data)
-// NOTE: this same issue is happening after running mapb a few times and going back to running map
-// DEBUG: first time I run mapb it goes up the map, and every time I run it after that it works as intended
-func cmdMapB() error {
-	fmt.Println("current URL *****************************************", currentUrl)
-	data, err := getPokeAPI(currentUrl, true)
+func cmdMapB(c *Config) error {
+	if c.Previous == nil {
+		return fmt.Errorf("You are already on the first page")
+	}
+	data, err := getPokeAPI(*c.Previous, true)
 	if err != nil {
 		return err
 	}
-	currentUrl = data.Previous
+	c.Previous = &data.Previous
+	c.Next = &data.Next
+
 	return nil
 }
 
@@ -113,6 +125,7 @@ func main() {
 	fmt.Println("Hello there!")
 	fmt.Println("Welcome to CLI Pokedex.")
 	fmt.Println("A CLI tool where you will be able to see and learn all about your favourite pokemons.")
+	config := &Config{}
 
 	fmt.Println("TEST") // NOTE: get rid of this
 
@@ -132,13 +145,13 @@ func main() {
 		// respond to user input
 		switch userInput {
 		case cliCmd["help"].name:
-			cliCmd["help"].callback()
+			cliCmd["help"].callback(config)
 		case cliCmd["exit"].name:
-			cliCmd["exit"].callback()
+			cliCmd["exit"].callback(config)
 		case cliCmd["map"].name:
-			cliCmd["map"].callback()
+			cliCmd["map"].callback(config)
 		case cliCmd["mapb"].name:
-			cliCmd["mapb"].callback()
+			cliCmd["mapb"].callback(config)
 		default:
 			fmt.Println("If you need help with the commands, please type in \"help\" and hit ENTER")
 		}
